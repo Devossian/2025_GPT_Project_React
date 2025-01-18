84e2a9e4c23d707d773cfabeb3cf4c62ee3fe7d7
JangInChan <simsim051128@gmail.com>
2025-01-19 07:32:49 +0900

    mod: 홈 화면 변경

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import '../styles/Home.css';
+
+const Home = () => {
+  return (
+    <div className="page">
+      <h1>어서오세요 Devossian-GPT 입니다.</h1>
+      <p>GPT를 더 싸게 이용하실수 있습니다!</p>
+    </div>
+  );
+};
+
+export default Home;
