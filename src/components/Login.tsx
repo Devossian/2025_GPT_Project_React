5e541617d3512f8a74b663f6b4d92594899dd1e3
kimbabZIP <02legend.park@gmail.com>
2025-02-14 14:10:37 +0900

    login 수정

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios, { AxiosError } from 'axios';
+import { AxiosError } from 'axios';
 import axiosInstance from "../api/axiosInstance";
 import '../styles/Login.css';
 
