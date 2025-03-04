bfe0ab5771b82ed33cbb5b225f4de139adb99036
kimbabZIP <02legend.park@gmail.com>
2025-03-04 12:40:00 +0900

    토큰 만료 로그아웃 수정

--- src/App.tsx
+++ src/App.tsx
@@ -18,20 +18,21 @@ const App = () => {
     }
   };
 
-  useEffect(() => {
+  useEffect(() => { // 1분마다 토큰 유효한지 확인
     const checkTokenExpiration = () => {
       const token = localStorage.getItem('token');
       if (token) {
         const decodedToken = parseJwt(token);
-        console.log(token);
         if (decodedToken && decodedToken.exp) {
-          console.log(decodedToken.exp);
           const expirationTime = decodedToken.exp * 1000 - 60000; // 만료 1분 전
           if (Date.now() >= expirationTime) {
             localStorage.removeItem('token');
             // 여기에 전역 상태 업데이트나 리다이렉션 등 추가 로그아웃 처리 로직을 넣으세요.
             alert('로그인 시간이 만료되어 로그아웃 됐습니다.');
           }
+        }
+        else{
+          localStorage.removeItem('token');
         }
       }
     };
